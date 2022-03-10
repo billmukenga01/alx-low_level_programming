a0c4f0b8f8e1ed80f44d0ff5ef92f02a5f8ddbda
billmukenga01 <66159940+billmukenga01@users.noreply.github.com>
2022-03-10 22:32:48 +0300

    Update printf.c

--- 0x00-hello_world/printf.c
+++ 0x00-hello_world/printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(void)
+	/**
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
