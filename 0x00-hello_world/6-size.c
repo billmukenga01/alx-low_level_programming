e7f8ff44ba93adaf3423b96ea806ec0f6f5f73a3
Bill Mukenga <billmukenga01@gmail.com>
2022-09-08 23:29:45 +0300

    made changes so as to conform to the betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-int main(void){
+
+/**
+*main - Entry point
+*Return always 0
+*/
+int main(void)
+{
     printf("Size of a char: %zu byte(s)\n", sizeof(char));
     printf("Size of a char: %zu byte(s)\n", sizeof(int));
     printf("Size of a char: %zu byte(s)\n", sizeof(long int));
     printf("Size of a char: %zu byte(s)\n", sizeof(long long int));
     printf("Size of a char: %zu byte(s)\n", sizeof(float));
-    return 0;
+    return (0);
 
 }
